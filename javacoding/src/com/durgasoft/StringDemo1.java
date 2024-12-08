07120dd444a1be8ebae56b6c19fdacbe619d9f3b
saakib_minhaj <saakib148@gmail.com>
2024-12-08 14:40:51 +0530

    string demo1

---
+++ javacoding/src/com/durgasoft/StringDemo1.java
@@ -0,0 +1,13 @@
+package com.durgasoft;
+
+public class StringDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = new String("saakib");
+		s.concat("minhaj");
+		System.out.println(s);
+
+	}
+
+}
