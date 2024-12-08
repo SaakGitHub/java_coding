9cdf5d5a94bfe6394699d96f04a53c87b111cb8a
saakib_minhaj <saakib148@gmail.com>
2024-12-08 14:44:57 +0530

    StringBuffer demo1

---
+++ javacoding/src/com/durgasoft/StringBufferDemo1.java
@@ -0,0 +1,16 @@
+package com.durgasoft;
+
+/**
+ * 
+ */
+public class StringBufferDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer buffer =new StringBuffer("Saima");
+		buffer.append("Saakib");
+		System.out.println(buffer);
+
+	}
+
+}
